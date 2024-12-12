442b5c13366c2cfb5b84a269730809b5a5e16b1a
sudhanshu20204 <sidshekhar2024@gmail.com>
2024-12-12 21:56:50 +0530

    add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,18 @@ type msgProp =
 
 const server = http.createServer((req, res) => {
   const URL = req.url;
+
+
+   res.setHeader("Access-Control-Allow-Origin", "*"); 
+   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+ 
+   if (req.method === "OPTIONS") {
+    
+     res.statusCode = 204;
+     res.end();
+     return;
+   }
   res.end("Server is requested on " + URL);
 });
 
